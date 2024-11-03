8b031803b55cd48da42bc8861a9fa0f058c8a98f
Mohammed <144555822+Mohammeddddd5@users.noreply.github.com>
2024-11-03 23:04:43 +0200

    Update Vole.cpp

--- Vole.cpp
+++ Vole.cpp
@@ -135,18 +135,94 @@ public:
             }
         }
     }
+    void Clear_Register(){
+        Registers.clear();
+    }
 };
 
 vector<pair<string, string>> Register::Registers; 
 
+
+
+class Memory: public ALU{
+private:
+    int CurrentStorage = 0;
+    string Memory[16][16] = {""};
+
+public:
+
+    void StoreValue(string Value, string Pattern){
+        vector<int> Location = identify_memory(Pattern);
+        Memory[Location[0]][Location[1]] = Value;
+    }
+
+
+    void StoreInstruction(string Instruction){
+        string first_2 = Instruction.substr(0, 2); 
+        string second_2 = Instruction.substr(2, 2);
+        int row1 = CurrentStorage / 16;
+        int col1 = CurrentStorage % 16;
+        Memory[row1+1][col1] = first_2;
+        int row2 = (CurrentStorage + 1) / 16;
+        int col2 = (CurrentStorage + 1) % 16;
+        Memory[row2+1][col2] = second_2;
+        CurrentStorage += 2;
+    }
+
+    string GetValue(string Pattern){
+        vector<int> Location = identify_memory(Pattern);
+        return Memory[Location[0]][Location[1]];
+    }
+    string GetValue(int row, int column) const {
+        return Memory[row][column];
+    }
+
+    void DisplayMemory(){
+        cout << "Memory: " << endl;
+        for(short i{0}; i < 16; i++){
+            cout << "[  ";
+            for(short j{0}; j < 16; j++){ 
+                if(Memory[i][j] == ""){
+                    cout << "00";
+                }
+                else{
+                    cout << Memory[i][j];
+                }
+                if (j < 15) cout << "   ";
+            }
+            cout << "  ]" << endl;
+        }
+    }
+    void Clear_Memory(){
+        for(short i{0}; i < 16; i++){
+            for(short j{0}; j < 16; j++){
+                Memory[i][j] = "00";
+            }
+        }
+    }
+};
+
+
 class CU : public ALU {
 public:
     void Execute_1RXY(Memory &memory, Register &Reg, const string &Instruction) {
-        
+        char registerChar = Instruction[1];
+        string memoryAddress = Instruction.substr(2, 2);
+        string value = memory.GetValue(memoryAddress);
+        string registerName = "R";
+        registerName += registerChar;
+        Reg.StoreRegister(registerName, value);
+        cout << "Executed 1RXY: Stored value " << value << " from memory address " << memoryAddress << " into register " << registerName << endl;
     }
 
+    
     void Execute_2RXY(Memory &memory, Register &Reg, const string &Instruction) {
-        
+        char registerChar = Instruction[1];
+        string immediateValue = Instruction.substr(2, 2);
+        string registerName = "R";
+        registerName += registerChar;
+        Reg.StoreRegister(registerName, immediateValue);
+        cout << "Executed 2RXY: Stored immediate value " << immediateValue << " in register " << registerName << endl;
     }
 
     void Execute_3RXY(Memory &memory, Register &Reg, const string &Instruction) {
@@ -162,16 +238,16 @@ public:
     }
 
     void Execute_6RST(Memory &memory, Register &Reg, const string &Instruction) {
-        int result;
-        cout << "Execution:" << endl;
-        string registerR = "R";
-        registerR.push_back(Instruction[1]);
-        string registerS = "R";
-        registerS.push_back(Instruction[2]);
-        string registerT = "R";
-        registerT.push_back(Instruction[3]);
-        string regSContent = Reg.GetRegisterContent(registerS);
-        string regTContent = Reg.GetRegisterContent(registerT);
+        // int result;
+        // cout << "Execution:" << endl;
+        // string registerR = "R";
+        // registerR.push_back(Instruction[1]);
+        // string registerS = "R";
+        // registerS.push_back(Instruction[2]);
+        // string registerT = "R";
+        // registerT.push_back(Instruction[3]);
+        // string regSContent = Reg.GetRegisterContent(registerS);
+        // string regTContent = Reg.GetRegisterContent(registerT);
     }
 
     void Execute_789RST(Register &Reg, const string Instruction){  // 712F
@@ -228,64 +304,12 @@ public:
 };
 
 
-class Memory: public ALU{
-private:
-    int CurrentStorage = 0;
-    string Memory[16][16] = {""};
-
-public:
-
-    void StoreValue(string Value, string Pattern){
-        vector<int> Location = identify_memory(Pattern);
-        Memory[Location[0]][Location[1]] = Value;
-    }
-
-
-    void StoreInstruction(string Instruction){
-        string first_2 = Instruction.substr(0, 2); 
-        string second_2 = Instruction.substr(2, 2);
-        int row1 = CurrentStorage / 16;
-        int col1 = CurrentStorage % 16;
-        Memory[row1][col1] = first_2;
-        int row2 = (CurrentStorage + 1) / 16;
-        int col2 = (CurrentStorage + 1) % 16;
-        Memory[row2][col2] = second_2;
-        CurrentStorage += 2;
-    }
-
-    string GetValue(string Pattern){
-        vector<int> Location = identify_memory(Pattern);
-        return Memory[Location[0]][Location[1]];
-    }
-
-    void DisplayMemory(){
-        cout << "Memory: " << endl;
-        for(short i{0}; i < 16; i++){
-            cout << "[  ";
-            for(short j{0}; j < 16; j++){ 
-                if(Memory[i][j] == ""){
-                    cout << "00";
-                }
-                else{
-                    cout << Memory[i][j];
-                }
-                if (j < 15) cout << "   ";
-            }
-            cout << "  ]" << endl;
-        }
-    }
-};
-
-
-
-
 class CPU : public ALU{
 public:
     string IR;
     Register Reg;
     CU Cu;
     int PC = 0;
-
     void SetRegister(Register& reg){
         Reg = reg;
     }
@@ -330,6 +354,12 @@ public:
             }
         }
     }
+    void DisplayRegisters(){
+        Reg.Display_Registers();
+    }
+    void clear_register(){
+        Reg.Clear_Register();
+    }
 };
 
 
@@ -339,14 +369,40 @@ class Vole_Machine : public ALU{
     private:
     CPU processor;
     Memory program_memory;
+    string Step;
     public:
-    void Execute_Program(string filepath){
-        fstream file(filepath, ios::in);
+    void StoreInstructions(string filepath){
         string line;
+        fstream file(filepath, ios::in);
         while(!file.eof()){
             getline(file, line);
             if(IsAValid_Instruction(line)){
-                processor.Execute_Step(line, program_memory);
+                program_memory.StoreInstruction(line);
+                processor.PC++;
+            }
+        }
+    }
+    void DisplayEverything(){
+        program_memory.DisplayMemory();
+        processor.DisplayRegisters();
+    }
+    void Restart(){
+        program_memory.Clear_Memory();
+        processor.clear_register();
+    }
+    void Execute_Program(){
+        for(int i{1}; i < 16; i++){
+            for(int j{0}; j < 16; j += 2){
+                Step.append(program_memory.GetValue(i,j));
+                Step.append(program_memory.GetValue(i,j+1));
+                cout << "Step: " << Step << endl;
+                if(IsAValid_Instruction(Step)){
+                    processor.Execute_Step(Step, program_memory);
+                }
+                else{
+                    return;
+                }
+                Step.clear();
             }
         }
     }
@@ -354,33 +410,75 @@ class Vole_Machine : public ALU{
 
 
 int main(){
-    Memory mem;
-    mem.StoreInstruction("102B");
-    mem.StoreInstruction("2096");
-    mem.StoreInstruction("2013");
-    mem.StoreInstruction("1919");
-    mem.StoreInstruction("1244");
-    mem.StoreValue("10","FF");
-    cout << "---------" << mem.GetValue("05");  
-
-    Register reg;
-    reg.StoreRegister("R0","32");
-    reg.StoreRegister("R4","A4");
-    reg.StoreRegister("R2","AB");
-    mem.DisplayMemory();
-    reg.Display_Registers();
-
-    cout << "Executing 7RST with instruction '____':" << endl;
-
-    CPU Cpu;
-    Cpu.SetRegister(reg);
-    Cpu.Execute_Step("7504", mem);
-    Cpu.Execute_Step("8504", mem);
-    Cpu.Execute_Step("9504", mem);
-
-    cout << "After Execution of ____:" << endl;
-    reg.Display_Registers();
-    mem.DisplayMemory();
-
-    return 0;
+    cout << "~ Vole Machine Program ~" << endl;
+    char answer;
+    string filename;
+    fstream file;
+    Vole_Machine Machine;
+    while(true){
+        cout << "1) Load Program" << endl;
+        cout << "2) Execute Program" << endl;
+        cout << "3) Restart" << endl;
+        cout << "4) Display Memory & Registers" << endl;
+        cout << "5) Exit" << endl;
+        cin >> answer;
+        switch(answer){
+            case '1':
+                cout << "Enter File name: ";
+                cin >> filename;
+                file.open(filename, ios::in);
+                while(file.fail()){
+                    cout << "Invalid input, enter a correct file name: ";
+                    cin >> filename;
+                    file.open(filename, ios::in);
+                }
+                Machine.StoreInstructions(filename);
+                break;
+            case '2':
+                Machine.Execute_Program();
+                break;
+            case '3':
+                Machine.Restart();
+                break;
+            case '4':
+                Machine.DisplayEverything();
+                break;
+            case '5':
+                exit(1);
+            default:
+                cout << "Invalid Answer, Enter a valid one";
+                cin >> answer;
+        }
+    }
 }
+
+
+    // Memory mem;
+    // mem.StoreInstruction("102B");
+    // mem.StoreInstruction("2096");
+    // mem.StoreInstruction("2013");
+    // mem.StoreInstruction("1919");
+    // mem.StoreInstruction("1244");
+    // mem.StoreValue("10","FF");
+    // cout << "---------" << mem.GetValue("05");  
+
+    // Register reg;
+    // reg.StoreRegister("R0","32");
+    // reg.StoreRegister("R4","A4");
+    // reg.StoreRegister("R2","AB");
+    // mem.DisplayMemory();
+    // reg.Display_Registers();
+
+    // cout << "Executing 7RST with instruction '____':" << endl;
+
+    // CPU Cpu;
+    // Cpu.SetRegister(reg);
+    // Cpu.Execute_Step("7504", mem);
+    // Cpu.Execute_Step("8504", mem);
+    // Cpu.Execute_Step("9504", mem);
+
+    // cout << "After Execution of ____:" << endl;
+    // reg.Display_Registers();
+    // mem.DisplayMemory();
+
+    // return 0;
